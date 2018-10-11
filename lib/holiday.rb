9f9c31300955e03b5e0f0e7b010f88c1a14a1c95
Scott M <gmat191@gmail.com>
2018-10-11 02:37:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! ":"
     holiday.each do |holiday, supplies|
-      
+      supplies = supplies.join("")
 end
 
 def all_holidays_with_bbq(holiday_hash)
